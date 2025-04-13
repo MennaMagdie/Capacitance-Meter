e0c583a5ad7cd2d208845bbeb96b425b32580fad
Menna Magdy <mennamagdyx@gmail.com>
2025-04-13 11:59:17 +0200

    cleaned repo

---
+++ ATmega16/AtmelStudioo/GccApplication2/GccApplication2/lcd.h
@@ -0,0 +1,18 @@
+/*
+ * lcd.h
+ *
+ * Created: 4/13/2025 11:53:24 AM
+ *  Author: Menna Magdy
+ */ 
+#ifndef LCD_H
+#define LCD_H
+
+void ms_spin(unsigned short ms);
+
+void lcd_init(void);
+void lcd_string(char *p, unsigned char pos);
+void lcd_clear(void);
+void hex2ascii(char *target, long value, char pointplace);
+char long2ascii(char *target, unsigned long value);
+
+#endif
